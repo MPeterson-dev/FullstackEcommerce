4d33f557196b78e17cf5ad48d04ee9d3fe0582c4
MPeterson-dev <MPeterson.Developer@outlook.com>
2025-02-04 15:44:47 -0700

    Products Router setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,16 @@
 import express from "express";
+import productsRoutes from "./routes/products/index";
 
 const app = express();
 const port = 3000;
 
+//Get = retrieve from API
 app.get("/", (req, res) => {
-  res.send("Hell Matthias");
+  res.send("Hello Matthias");
 });
+
+// Routes
+app.use("/products", productsRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
