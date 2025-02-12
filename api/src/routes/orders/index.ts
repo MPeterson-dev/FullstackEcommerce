669498faeb9876d3a72296914febe2e9d21c3674
MPeterson-dev <MPeterson.Developer@outlook.com>
2025-02-12 16:18:45 -0700

    Orders CRUD

--- api/src/routes/orders/index.ts
+++ api/src/routes/orders/index.ts
@@ -1,6 +1,14 @@
 import { Router } from "express";
-import { createOrder } from "./ordersController.js";
-import { insertOrderWithItemsSchema } from "../../db/ordersSchema.js";
+import {
+  createOrder,
+  listOrders,
+  getOrder,
+  updateOrder,
+} from "./ordersController.js";
+import {
+  insertOrderWithItemsSchema,
+  updateOrderSchema,
+} from "../../db/ordersSchema.js";
 import { validateData } from "../../middlewares/validationMiddleware.js";
 import { verifyToken } from "../../middlewares/authMiddleware.js";
 
@@ -12,5 +20,9 @@ router.post(
   validateData(insertOrderWithItemsSchema),
   createOrder
 );
+
+router.get("/", verifyToken, listOrders);
+router.get("/:id", verifyToken, getOrder);
+router.put("/:id", verifyToken, validateData(updateOrderSchema), updateOrder);
 
 export default router;
