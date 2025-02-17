576b18a37ef44a7e5a5cad5a027878f65927eeef
MPeterson-dev <MPeterson.Developer@outlook.com>
2025-02-17 12:49:28 -0700

    Create expo app.

--- ecommerce-mobile/components/ProductListItem.tsx
+++
@@ -1,5 +0,0 @@
-import { Text } from "react-native";
-
-export default function ProductListItem({ product }) {
-  return <Text style={{ fontSize: 30 }}>{product.name}</Text>; // Displays the 'name' property of each product in large text
-}
